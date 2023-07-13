597e7668fa13a21713e320b00dd0758335900e2b
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2023-07-13 17:30:15 +0600

    customer apis done

--- controllers/Customer.js
+++ controllers/Customer.js
@@ -2,6 +2,9 @@ import httpStatus from "http-status";
 import Customer from "../models/Customer.js";
 import catchAsync from "../shared/catchAsync.js";
 import { responseObj } from "../shared/response.js";
+import { ApiError } from "../errors/ApiError.js";
+import mongoose from "mongoose";
+import Order from "../models/Order.js";
 
 export const createCustomer = catchAsync(async (req, res) => {
 	const customer = req.body;
@@ -11,6 +14,89 @@ export const createCustomer = catchAsync(async (req, res) => {
 		httpStatus.CREATED,
 		"Customer is created successfully",
 		savedCustomer
+	);
+	res.json(result);
+});
+
+export const updateCustomer = catchAsync(async (req, res) => {
+	const data = req.body;
+	const id = req.params.id;
+	const updateData = await Customer.findOneAndUpdate({ _id: id }, data, {
+		new: true,
+	});
+	const result = responseObj(
+		httpStatus.CREATED,
+		"Customer updated successfully",
+		updateData
+	);
+	res.json(result);
+});
+
+export const getSingleCustomer = catchAsync(async (req, res) => {
+	const customerId = req.params.id;
+	const customer = await Customer.findById(customerId);
+
+	if (!customer) {
+		throw new ApiError(httpStatus.NOT_FOUND, "Customer not found");
+	}
+
+	const result = responseObj(
+		httpStatus.OK,
+		"Customer retrieved successfully",
+		customer
+	);
+	res.json(result);
+});
+
+export const deleteCustomer = catchAsync(async (req, res) => {
+	const customerId = req.params.id;
+	console.log('customerId: ', customerId)
+
+	const _pipeline = [
+		{
+			$match: {
+				_id: new mongoose.Types.ObjectId(customerId)
+			}
+		},
+		{
+			$lookup: {
+				from: 'orders', // actual name of the related collection
+				localField: '_id', // customer _id of this collection
+				foreignField: 'customerId', // customer _id of orders collection
+				as: 'orders' // key of where the response will come
+			}
+		}
+	]
+	const _deleteCustomerList = await Customer.aggregate(_pipeline)
+
+	if (!_deleteCustomerList.length) {
+		throw new ApiError(httpStatus.NOT_FOUND, "Customer not found")
+	} else {
+		const _orderIds = _deleteCustomerList[0].orders.map((_order) => _order._id)
+		const _deletedCustomer = await Customer.findByIdAndDelete(customerId)
+		
+		console.log('will delete orders length: ', _orderIds.length)
+
+		for (const _orderId of _orderIds) {
+			await Order.findByIdAndDelete(_orderId)
+		}
+
+		const result = responseObj(
+			httpStatus.OK,
+			"Customer and related Orders deleted successfully",
+			"deletedCustomer"
+		);
+		res.json(result);
+	}
+});
+
+export const getAllCustomers = catchAsync(async (req, res) => {
+	const customers = await Customer.find();
+	console.log(customers);
+	const result = responseObj(
+		httpStatus.OK,
+		"Customers retrieved successfully",
+		customers
 	);
 	res.json(result);
 });
--- routes/customer.js
+++ routes/customer.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { createCustomer } from "../controllers/Customer.js";
+import { createCustomer,
+	deleteCustomer,
+	getAllCustomers,
+	getSingleCustomer,
+	updateCustomer, } from "../controllers/Customer.js";
 
 const customerRoute = express.Router();
 
-customerRoute.route("/").post(createCustomer);
+customerRoute.route("/").post(createCustomer).get(getAllCustomers);
+customerRoute
+	.route("/:id")
+	.patch(updateCustomer)
+	.delete(deleteCustomer)
+	.get(getSingleCustomer);
 
 export { customerRoute };
+  
